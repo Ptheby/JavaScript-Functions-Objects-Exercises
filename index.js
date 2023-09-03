80d542ba677b1b7bf815421510bcd15a2489cfa3
Paul Theby <paultheby@gmail.com>
2023-09-03 18:01:30 -0500

    Completed Exercise 5: Advanced Object Methods

--- index.js
+++ index.js
@@ -54,8 +54,50 @@ let student = {
   student.print();
 
   //Walker is 5 years old and is in Kindergarten
-  
+
   ///here i create an object named student with the attributes or properties--key:values of name: walker, age:5 and grade:kindergarten,
   //as a part of that object i create function called "print" and that function console logs with the `${ to pass in the values from
   //the current object using "this" method and passing the values from the corresponding keys.  I am just not sure why the 
   //strings do not need to be in quotations. 
+
+  ///5 Exercise 5: Advanced Object Methods
+  //Implement an object named library with properties for books (as an array) and methods to addBook(bookName) and removeBook(bookName).
+//   let library= {
+//     books:["It","The Shining", "The Stand"],
+//     addBook(bookName) {
+//         this.books.push(bookName)
+//     },
+//     removeBook(bookName) {
+//         this.books.remove(bookName)
+//     }
+//   };
+//   library.addBook("The Pet Semetary");
+//   console.log(library);
+//   library.removeBook( "It");
+//   console.log(library);
+//   break at 1:13  ///this isn't working for remove after multiple attempts and help from google and trying to find other methods. 
+
+
+let library = {
+    books: ["It", "The Shining", "The Stand"],
+    addBook(bookName) {
+      this.books.push(bookName);
+    },
+    removeBook(bookName) {
+      const indexToRemove = this.books.indexOf(bookName);
+      if (indexToRemove !== -1) {
+        this.books.splice(indexToRemove, 1);
+      }
+    }
+  };
+  
+  library.addBook("The Pet Semetary");
+  console.log(library);
+  library.removeBook("It");
+  console.log(library);
+////this works but i barely know how. I kind of understand the add book with push method. We created an object library with 
+//an array labeled books with three book titles, next in the object we created a function? called addBook with a parameter of 
+//and an method of push on this(current object).books(property of current object library)so that we can then add a new bookName
+//beblow when i invoke the function buy using the library.addBook("Pet SEmatary")passing Pet Semetary into the argument.
+
+  
